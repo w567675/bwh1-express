10fb8e2b506d3564dadefeaa73e5668e9e5d35e6
zhanghaiyang <403724532@qq.com>
2017-08-15 09:11:32 +0800

    ts配置

---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+import express  = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Express' });
+});
+export default router;
